817dddb5fb077c4b2ca9efd9fd503ffff41c0dee
dcarruth <car12047@byui.edu>
2018-03-07 23:15:23 -0700

    Functional

--- prove08.js
+++ prove08.js
@@ -8,6 +8,57 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/getRate', (req, res) => {
-	res.render('result');
+	
+	var weight = Number(req.query.weight);
+	var type = String(req.query.type);
+	var cost = calculateRate(weight, type);
+	res.render('result', {
+		weight: weight,
+		type: type,
+		cost: calculateRate(weight, type).toFixed(2)
+	});
   })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  
+
+function calculateRate(weight, type){
+	var cost = 0.0;
+	switch (type){
+		case "Letters (Stamped)":
+			if (weight <= 1){
+				cost = .50;
+			}	
+			else {
+				cost = .50 + (.21 * (Math.ceil(weight) - 1));
+			}
+			break;
+		case "Letters (Metered)":
+			if (weight <= 1){
+				cost = .47;
+			}	
+			else {
+				cost = .47 + (.21 * (Math.ceil(weight) - 1));
+			}
+			break;
+		case "Large Envelopes (Flats)":
+			if (weight <= 1){
+				cost = 1.00;
+			}	
+			else {
+				cost = 1.00 + (.21 * (Math.ceil(weight) - 1));
+			}
+			break;
+		case "Parcels": 
+			if (weight <= 4){
+				cost = 3.50;
+			}	
+			else if (weight <= 8) {
+				cost = 3.75;
+			}
+			else {
+				cost = 3.75	 + (.35 * (Math.ceil(weight) - 8));
+			}
+			break;
+		}
+	return cost;
+}
