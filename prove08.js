97073cac9b3d4d74e85e2ed6cae11f7936c5244f
dcarruth <car12047@byui.edu>
2018-03-07 21:23:34 -0700

    asd

--- prove08.js
+++ prove08.js
@@ -6,7 +6,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/prove08'))
+  .get('/', (req, res) => res.render('pages/index'))
   .get('/getRate', (req, res) => {
 	res.render('result');
   })
